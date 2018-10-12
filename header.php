d180e5a2d8ff8410c880effac0c7199c1ce86f18
long-lou <44078267+long-lou@users.noreply.github.com>
2018-10-12 11:19:34 +0800

    change title

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Students system</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <header>
+            <a href="students.php">Home</a>
+            <a href="addstudents.php">Add student</a>
+            <a href="logout.php">Logout</a>
+        </header>
